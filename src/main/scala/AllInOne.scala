44221d252620cd426e912d823925db467606fa69
Axel <axel.gschaider@gmail.com>
2014-04-30 15:30:03 +0200

    exercise 10

--- src/main/scala/AllInOne.scala
+++ src/main/scala/AllInOne.scala
@@ -1,4 +1,32 @@
 
+case class State[S, +A](run:S => (A,S)) {
+
+	import State._
+
+	def flatMap[B](g: A => State[S,B]):State[S,B] = 
+		State( s => {
+			val (a,s2) = run(s)
+			g(a) run s2
+		} )
+
+	def map[B](f:A=>B):State[S,B] = flatMap( f andThen unit )
+
+	def map2[B,C]( sb:State[S,B] )(f:(A,B) => C):State[S,C] =
+		flatMap(a => sb.map( f(a,_) ) )
+
+}
+
+object State {
+	def unit[S,A](a:A):State[S,A] = State( s => (a,s) )
+
+	def prepend[S,A](s:State[S,A], to:State[S, List[A]]):State[S, List[A]] = 
+		s.map2(to){_ :: _}
+
+	//def sequence[S,A](l:List[State[S,A]]):State[S, List[A]] =
+	//	l.foldRight( unit(List.empty[A]) ){ case (s,to) => prepend _ }
+
+}
+
 trait RNG {
 	def nextInt:(Int, RNG)
 }
@@ -6,78 +34,52 @@ trait RNG {
 
 object RNG {
 
-	type Rand[+A] = RNG => (A, RNG)
+	type Rand[+A] = State[RNG, A]
 
-	val int: Rand[Int] = _.nextInt
+	val int: Rand[Int] = State(_.nextInt)
 
-	def unit[A](a:A):Rand[A] =  (a, _)
-
-	def map[A,B](s:Rand[A])(f:A=>B):Rand[B] = flatMap(s)( f andThen unit )
-
-	def map2[A,B,C](ra:Rand[A], rb:Rand[B])(f:(A,B) => C):Rand[C] = 
-		flatMap(ra){a => map(rb)( f(a,_) ) }
-
-	def flatMap[A,B](f: Rand[A])(g: A => Rand[B]):Rand[B] = 
-		rng => {
-			val (a, rng1) = f(rng)
-			g(a)(rng1)
-		}
-
-	def both[A,B](ra:Rand[A], rb:Rand[B]):Rand[(A,B)] = map2(ra,rb)((_,_))
+	def both[A,B](ra:Rand[A], rb:Rand[B]):Rand[(A,B)] = ra.map2(rb)((_,_))
 
 	def nonNegativeEven:Rand[Int] = 
-		map(nonNegativeInt)( i => i - i % 2 )
+		nonNegativeInt.map( i => i - i % 2 )
 	
-	def nonNegativeInt(rng:RNG):(Int,RNG) = {
-		val (i,newRng) = rng.nextInt
-		val posI = if(i >= 0) i
-		           else ( i + 1 ) * (-1)
-		(posI, newRng) 
-	}
+	def nonNegativeInt:Rand[Int] = 
+		State( { rng:RNG =>
+			val (i,newRng) = rng.nextInt
+			val posI = if(i >= 0) i
+		               else ( i + 1 ) * (-1)
+			(posI, newRng) 
+			} )
+		
 
-	def biggerThanZero(rng:RNG):(Int,RNG) = {
-		val (i,newRng) = nonNegativeInt(rng)
-		if(i == 0) biggerThanZero(newRng)
-		else       (i, newRng)
-	}
+	def biggerThanZero:Rand[Int] = 
+		State( {rng:RNG =>
+			val (i,newRng) = nonNegativeInt run rng
+			if(i == 0) biggerThanZero run newRng
+			else       (i, newRng)
+		})
 
 	val double:Rand[Double] =
-		map(biggerThanZero)(i => (i-1).toDouble / Int.MaxValue.toDouble)
+		biggerThanZero.map(i => (i-1).toDouble / Int.MaxValue.toDouble)
 
-	val randIntDouble:Rand[(Int,Double)] = both(int, double)
-	val randDoubleInt:Rand[(Double,Int)] = both(double, int)
+	val intDouble:Rand[(Int,Double)] = both(int, double)
+	val doubleInt:Rand[(Double,Int)] = both(double, int)
 	
-	def intDouble(rng:RNG):((Int,Double), RNG) = {
-		val (i, rng1) = rng.nextInt
-		val (d, rng2) = double(rng1)
+	def double2:Rand[(Double, Double)] = both(double, double)
 
-		( (i,d), rng2 )
-	}
+	def double3:Rand[(Double, Double, Double)] = 
+		double2.map2( double ){ case ( (a,b), c) => (a,b,c) }
+	
 
-	def doubleInt(rng:RNG):((Double,Int),RNG) = intDouble(rng) match {
-		case ( (i,d),r ) => ( (d,i),r )
-	}
+	//def sequence[A](fs:List[Rand[A]]):Rand[List[A]] =
+	//	fs.foldRight( State.unit( List.empty[A] ) )( State.prepend _ )
 
-	def double3(rng:RNG):((Double,Double,Double),RNG) = {
-		val (d1,r1) = double(rng)
-		val (d2,r2) = double(r1)
-		val (d3,r3) = double(r2)
-
-		( (d1,d2,d3), r3 )
-	}
-
-	def prepend[A]( rng:Rand[A], to:Rand[List[A]] ):Rand[List[A]] =
-		map2(rng, to){ _ :: _ }
-
-	def sequence[A](fs:List[Rand[A]]):Rand[List[A]] =
-		fs.foldRight( unit( List.empty[A] ) )( prepend _ )
-
-	def ints(count:Int):Rand[List[Int]] = sequence( List.fill(count)(int) )
+	//def ints(count:Int):Rand[List[Int]] = sequence( List.fill(count)(int) )
 
 	def nonNegativeLessThan(n:Int):Rand[Int] = 
-		flatMap(nonNegativeInt){ i => 
+		nonNegativeInt flatMap { i => 
 			val mod = i % n
-			if (i + (n-1) - mod >= 0) unit(mod)
+			if (i + (n-1) - mod >= 0) State.unit(mod)
 			else nonNegativeLessThan(n)
 		}
 
