fd91182d6e978507d06fdedc792f3af7948fc38f
Axel <axel.gschaider@gmail.com>
2014-04-30 10:24:03 +0200

    exercise 6

--- src/main/scala/AllInOne.scala
+++ src/main/scala/AllInOne.scala
@@ -18,6 +18,15 @@ object RNG {
 			(f(a), rng2)
 		}
 
+	def map2[A,B,C](ra:Rand[A], rb:Rand[B])(f:(A,B) => C):Rand[C] = 
+		rng => {
+			val (a,rng2) = ra(rng)
+			val (b,rng3) = rb(rng2)
+			( f(a,b), rng3 )
+		}
+
+	def both[A,B](ra:Rand[A], rb:Rand[B]):Rand[(A,B)] = map2(ra,rb)((_,_))
+
 	def nonNegativeEven:Rand[Int] = 
 		map(nonNegativeInt)( i => i - i % 2 )
 	
