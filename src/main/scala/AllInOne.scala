dbb1b3876e7b98c5adc4c564223486d4b9d726fd
Axel <axel.gschaider@gmail.com>
2014-04-30 02:46:09 +0200

    ugly API

--- src/main/scala/AllInOne.scala
+++ src/main/scala/AllInOne.scala
@@ -10,6 +10,52 @@ object RngUser {
 		           else ( i + 1 ) * (-1)
 		(posI, newRng) 
 	}
+
+	def biggerThanZero(rng:RNG):(Int,RNG) = {
+		val (i,newRng) = nonNegativeInt(rng)
+		if(i == 0) biggerThanZero(newRng)
+		else       (i, newRng)
+	}
+
+	def double(rng:RNG):(Double, RNG) = {
+		val (x, rng1) = biggerThanZero(rng)
+		val (y, rng2) = biggerThanZero(rng1)
+		val (divisor, dividend) =
+			if(x == y)     (x, x-1)
+			else if(x < y) (y, x)
+			else           (x, y)
+
+		val d = dividend.toDouble / divisor.toDouble
+		(d, rng2)
+	}
+
+	def intDouble(rng:RNG):((Int,Double), RNG) = {
+		val (i, rng1) = rng.nextInt
+		val (d, rng2) = double(rng1)
+
+		( (i,d), rng2 )
+	}
+
+	def doubleInt(rng:RNG):((Double,Int),RNG) = intDouble(rng) match {
+		case ( (i,d),r ) => ( (d,i),r )
+	}
+
+	def double3(rng:RNG):((Double,Double,Double),RNG) = {
+		val (d1,r1) = double(rng)
+		val (d2,r2) = double(r1)
+		val (d3,r3) = double(r2)
+
+		( (d1,d2,d3), r3 )
+	}
+
+	def ints(count:Int)(rng:RNG):(List[Int],RNG) = 
+		if(count <= 0) (Nil,rng)
+		else {
+			val (i ,r1) = rng.nextInt
+			val (is,r2) = ints( count - 1 )(r1)
+			(i::is, r2)
+		}
+	
 }
 
 case class Simple(seed:Long) extends RNG {
